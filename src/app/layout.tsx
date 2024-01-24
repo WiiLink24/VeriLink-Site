8682d9a3f83d0c5adf52263e989da6f7b09d0211
Matthew <github-gpg@service.matthe815.dev>
2024-01-23 22:32:35 -0500

    first-commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Verilink",
+  description: "Verify your Discord account with ease.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">
+        Verilink
+      </h1>
+      <p className="mt-3 text-2xl">
+        Verify your Discord account with ease.
+      </p>
+    </div> 
+  )
+}
