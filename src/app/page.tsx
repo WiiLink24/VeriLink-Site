245a2c67bc2a58d86301116a357e2be22fcd0728
Matthew <github-gpg@service.matthe815.dev>
2024-01-23 22:44:08 -0500

    feature: Add recapcha support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+"use client"
+
 import Image from "next/image";
+import { useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function Home() {
+  const recaptchaRef = useRef<ReCAPTCHA>(null)
+  const [isVerified, setIsverified] = useState(false);
+  async function handleCaptchaSubmission(token: string | null) {
+    console.log(token)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-6xl font-bold">
@@ -9,6 +19,10 @@ export default function Home() {
       <p className="mt-3 text-2xl">
         Verify your Discord account with ease.
       </p>
+      <ReCAPTCHA 
+        sitekey="6LeopFopAAAAAHFNz2wZRzWzkVHrrAvqc-oNzHkl"
+        onChange={handleCaptchaSubmission}
+      />
     </div> 
   )
 }
